9da94db3c7f75e1b0e24eca02c16a1bcce7ec322
William Ferreira Dias <will_maverick10@hotmail.com>
2022-12-24 02:59:31 -0300

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+
+  images: {
+    domains: [
+      'files.stripe.com',
+    ],
+  },
+
+  experimental: {
+    images: {
+      allowFutureImage: true,
+    },
+  },
+
+
 }
+
+
 
 module.exports = nextConfig
