2636ddf7a7fe2849218058dfa445d76b96e439d8
William Ferreira Dias <will_maverick10@hotmail.com>
2022-12-29 16:52:00 -0300

    finalizando

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,21 +1,51 @@
+import axios from "axios"
 import { GetStaticPaths, GetStaticProps } from "next"
 import Image from "next/image"
 import { useRouter } from "next/router"
+import { useState } from "react"
 import Stripe from "stripe"
 import { stripe } from "../../lib/stripe"
 import { ImageContainer, ProductContainer, ProductDetails } from "../../styles/pages/product"
 
 interface ProductProps {
     product: {
-        id: string
-        name: string
-        imageUrl: string
-        price: string
-        description: string
+        id: string;
+        name: string;
+        imageUrl: string;
+        price: string;
+        description: string;
+        defaultPriceId: string;
       }
 }
+
 export default function Product({ product }: ProductProps) {
-    const { query } = useRouter()
+   /* const { isFallback } = useRouter()
+
+    if (isFallback) {
+        return <p>Loading...</p>
+    }*/
+    const [isCreatingCheckoutSession, setIsCreatingCheckoutSession] = useState(false);
+
+    async function handleBuyButton() {
+        try {
+
+            setIsCreatingCheckoutSession(true);
+
+            const response = await axios.post('/api/checkout', {
+                priceId: product.defaultPriceId
+            })
+
+            const { checkoutUrl } = response.data;
+
+            window.location.href = checkoutUrl
+
+        } catch (error) {
+
+            setIsCreatingCheckoutSession(false);
+            alert('Falha ao redirecionar ao checkout!')
+        }
+    }
+
     return (
         <ProductContainer>
             <ImageContainer>
@@ -27,23 +57,25 @@ export default function Product({ product }: ProductProps) {
                 <span>{product.price}</span>
 
                 <p>{product.description}</p>
-                <button>Comprar</button>
+                <button disabled={isCreatingCheckoutSession} onClick={handleBuyButton}>
+                    Comprar
+                </button>
             </ProductDetails>
         </ProductContainer>
     )
 }
-/*
+
 export const getStaticPaths: GetStaticPaths = async () => {
-    return (
+    return {
         paths: [
-           // { params: { id: } }
+            { params: { id: 'prod_N2HghXgXE6gXuV'} }
         ],
-        fallback: true,
-    )
-}*/
+        fallback: 'blocking',
+    }
+}
 
 export const getStaticProps: GetStaticProps<any, { id: string }> = async ({ params }) => {
-    const productId = params.id;
+    const productId = params!.id;
 
     const product = await stripe.products.retrieve(productId, {
         expand: ['default_price'],
@@ -61,6 +93,8 @@ export const getStaticProps: GetStaticProps<any, { id: string }> = async ({ para
                   style: 'currency',
                   currency: 'BRL',
                 }).format(price.unit_amount! / 100),
+                description: product.description,
+                defaultPriceId: price.id
               }
         },
         revalidate: 60 * 60 * 1, //1 hour
--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,5 +1,57 @@
-export default function Success() {
+import { GetServerSideProps } from "next";
+import Image from "next/image";
+import Link from "next/link";
+import Stripe from "stripe";
+import { stripe } from "../lib/stripe";
+import { ImageContainer, SuccessContainer } from "../styles/pages/success";
+
+interface SuccessProps {
+    costumerName: string;
+    product: {
+        name: string;
+        imageUrl: string;
+    }
+}
+
+export default function Success({ costumerName, product }: SuccessProps) {
     return (
-        <h1>Success</h1>
+        <SuccessContainer>
+            <h1>Compra efetuada!</h1>
+
+            <ImageContainer>
+               <Image src={product.imageUrl} width={120} height={110} alt=""/>
+
+            </ImageContainer>
+
+            <p>
+                    Urrull <strong>{costumerName}</strong>, sua camisa <strong>{product.name}</strong> esta a caminho.
+                </p>
+            
+            <Link href="/">
+                Voltar ao catálogo
+            </Link>
+
+        </SuccessContainer>
     )
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+    const sessionId = String(query.session_id);
+  
+    const session = await stripe.checkout.sessions.retrieve(sessionId, {
+      expand: ['line_items', 'line_items.data.price.product']
+    });
+  
+    const costumerName = session.customer_details!.name;
+    const product = session.line_items!.data[0].price!.product as Stripe.Product;
+  
+    return {
+      props: {
+        costumerName,
+        product: {
+          name: product.name,
+          imageUrl: product.images[0]
+        }
+      }
+    }
 }
